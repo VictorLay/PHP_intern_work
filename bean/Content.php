cb3687b77ae96660b3c80206f5195766b682eda6
VictorLay <viktor.latyshey@mail.ru>
2022-07-19 18:00:49 +0300

    Added new entity classes "Course" & "Content"
    Добавлены сущности.

---
+++ bean/Content.php
@@ -0,0 +1,88 @@
+<?php
+
+class Content implements JsonSerializable
+{
+    private array $text;
+    private array $linkToTheArticle;
+    private array $linkToTheVideo;
+
+    /**
+     * @param array $text
+     * @param array $linkToTheArticle
+     * @param array $linkToTheVideo
+     */
+    public function __construct(array $text = [], array $linkToTheArticle = [], array $linkToTheVideo = [])
+    {
+        $this->text = $text;
+        $this->linkToTheArticle = $linkToTheArticle;
+        $this->linkToTheVideo = $linkToTheVideo;
+    }
+
+    /**
+     * @return array
+     */
+    public function getText(): array
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param array $text
+     */
+    public function setText(array $text): void
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLinkToTheArticle(): array
+    {
+        return $this->linkToTheArticle;
+    }
+
+    /**
+     * @param array $linkToTheArticle
+     */
+    public function setLinkToTheArticle(array $linkToTheArticle): void
+    {
+        $this->linkToTheArticle = $linkToTheArticle;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLinkToTheVideo(): array
+    {
+        return $this->linkToTheVideo;
+    }
+
+    /**
+     * @param array $linkToTheVideo
+     */
+    public function setLinkToTheVideo(array $linkToTheVideo): void
+    {
+        $this->linkToTheVideo = $linkToTheVideo;
+    }
+
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'text' => $this->text,
+            'linkToTheArticle' => $this->linkToTheArticle,
+            'linkToTheVideo' => $this->linkToTheVideo];
+    }
+
+    public function setContentFieldsWithJson(string $jsonContent): void
+    {
+        $array = json_decode($jsonContent, JSON_OBJECT_AS_ARRAY);
+
+        $this->text = $array['text'];
+        $this->linkToTheArticle = $array['linkToTheArticle'];
+        $this->linkToTheVideo = $array['linkToTheVideo'];
+    }
+
+
+}
--- bean/Course.php
+++ bean/Course.php
@@ -1,18 +1,82 @@
 <?php
+require_once "./bean/Content.php";
 
 class Course extends Entity
 {
 
     private string $title;
 
-    private string $author;
+    private int $authorId;
+
+    private Content $content;
+
+    /**
+     * @param string $title
+     * @param int $authorId
+     * @param Content $content
+     */
+    public function __construct(string $title, int $authorId, Content $content)
+    {
+        $this->title = $title;
+        $this->authorId = $authorId;
+        $this->content = $content;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAuthorId(): int
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * @param int $authorId
+     */
+    public function setAuthorId(int $authorId): void
+    {
+        $this->authorId = $authorId;
+    }
+
+
+    /**
+     * @return Content
+     */
+    public function getContent(): Content
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param Content $content
+     */
+    public function setContent(Content $content): void
+    {
+        $this->content = $content;
+    }
+
+
+
 //У курса есть название
 //У курса есть автор
 //У курса есть контент, который будет хранится в json
 
-//возможен контент разных типов:
-//- текст
-//- ссылка на статью
-//- ссылка на видео
 
 }
